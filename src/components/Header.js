83e9c21e3c617ac6193e62698272041921ed635e
hichambenaalioua <ali.hicham1997@gmail.com>
2023-04-24 20:05:17 +0100

    more fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import image from "../images/d.jpg";
 import video from "../images/video.mp4";
 function Header() {
   return (
-    <div className=" w-full h-[32rem]  text-white flex flex-col items-center justify-center ">
+    <div className=" w-screen h-[44rem]  text-white flex flex-col items-center justify-center ">
       <video
-        className="object-cover w-full h-[32rem] -z-10 absolute"
+        className="object-cover w-screen h-[44rem] -z-10 absolute"
         autoPlay
         loop
         muted
