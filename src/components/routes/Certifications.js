ccc6bc5753445a4d29bc9b086bf5880e9afae119
hichambenaalioua <ali.hicham1997@gmail.com>
2023-04-24 18:29:13 +0100

    add certifications

--- src/components/routes/Certifications.js
+++ src/components/routes/Certifications.js
@@ -2,38 +2,31 @@ import React from "react";
 
 const certifications = [
   {
-    nom: "certificat 1",
-    description: "lorem ipsum dolor sit amet, consectetur adipis",
-    path: "image1.jpg",
+    nom: "Python Functions, Files, and Dictionaries",
+    number: 1,
+    instutition: "University of Michigan",
     link: "https://coursera.org/share/d2d1403d15c47bb0804003ed0da405ca",
     anne: "2022",
   },
   {
-    nom: "certificat 2",
-    description: "lorem ipsum dolor sit amet, consectetur adipis",
-    path: "image1.jpg",
-    link: "https://coursera.org/share/d2d1403d15c47bb0804003ed0da405ca",
-    anne: "2022",
+    nom: "DevOps, Cloud, and Agile Foundations",
+    number: 2,
+    instutition: "IBM",
+    link: "https://www.coursera.org/account/accomplishments/specialization/ABW7AGCHD6CR",
+    anne: "2023",
   },
   {
-    nom: "certificat 3",
-    description: "lorem ipsum dolor sit amet, consectetur adipis",
-    path: "image1.jpg",
-    link: "https://coursera.org/share/d2d1403d15c47bb0804003ed0da405ca",
-    anne: "2022",
+    nom: "Developing Back-End Apps with Node.js and Express",
+    number: 3,
+    instutition: "IBM",
+    link: "https://www.coursera.org/account/accomplishments/verify/V82FFRN7GDWC",
+    anne: "2023",
   },
   {
-    nom: "certificat 4",
-    description: "lorem ipsum dolor sit amet, consectetur adipis",
-    path: "image1.jpg",
-    link: "https://coursera.org/share/d2d1403d15c47bb0804003ed0da405ca",
-    anne: "2022",
-  },
-  {
-    nom: "certificat 5",
-    description: "lorem ipsum dolor sit amet, consectetur adipis",
-    path: "image1.jpg",
-    link: "https://coursera.org/share/d2d1403d15c47bb0804003ed0da405ca",
+    nom: "Data Collection and Processing with Python",
+    number: 4,
+    instutition: "University of Michigan",
+    link: "https://www.coursera.org/account/accomplishments/verify/MH362J786KL4",
     anne: "2022",
   },
 ];
@@ -45,12 +38,12 @@ function Certifications() {
         {certifications.map((certification, index) => (
           <div
             key={index}
-            class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            class="w-[500px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
           >
             <a href={certification.link} target="_blank" rel="noreferrer">
               <img
                 class="rounded-t-lg"
-                src={require("../../images/certif1.jpeg")}
+                src={require(`../../images/certif${certification.number}.jpeg`)}
                 alt=""
               />
             </a>
@@ -61,7 +54,7 @@ function Certifications() {
                 </h5>
               </a>
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {certification.description}
+                {certification.instutition}
               </p>
               <h5 class="mb-2 text-1xl font-light tracking-tight text-gray-900 dark:text-white">
                 {certification.anne}
