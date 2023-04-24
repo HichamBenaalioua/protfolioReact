0577cea82636f2aa21836eca908a1e8fd27b39de
hichambenaalioua <ali.hicham1997@gmail.com>
2023-04-24 19:50:23 +0100

    added background video and experience

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
 import React from "react";
 import image from "../images/d.jpg";
+import video from "../images/video.mp4";
 function Header() {
   return (
-    <div className=" w-screen h-[32rem] bg-gray-800 text-white flex flex-col items-center justify-center ">
+    <div className=" w-screen h-[32rem]  text-white flex flex-col items-center justify-center ">
+      <video
+        className="object-cover w-screen h-[32rem] -z-10 absolute"
+        autoPlay
+        loop
+        muted
+      >
+        <source src={video} type="video/mp4" />
+      </video>
       <div className="flex flex-col justify-evenly items-center h-[380px]">
         <img
           className="object-cover w-56 h-56 rounded-full "
